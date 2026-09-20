f494eaf518224425114c3845af6aae6c04659091
bitu-developer <bitu.network@gmail.com>
2026-09-20 22:26:17 +0330

    rename src/web/assets to src/http/assets

---
+++ src/http/assets/components.js
@@ -0,0 +1,2 @@
+// file: src/http/assets/components.js
+import "./globe/globe-view.js";
---
+++ src/http/assets/globe/globe.test.js
@@ -0,0 +1,11 @@
+// file: src/http/assets/globe/globe.test.js
+import { test, expect } from '@playwright/test';
+
+test('renders cesium-globe element', async ({ page }) => {
+  await page.setContent(`
+    <script type="module" src="./globe.js"></script>
+    <cesium-globe style="width: 400px; height: 400px;"></cesium-globe>
+  `);
+  const globe = page.locator('cesium-globe');
+  await expect(globe).toBeVisible();
+});
