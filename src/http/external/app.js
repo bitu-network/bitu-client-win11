8a342180e183e1171aeeb9977d2f7d89e4b31093
bitu-developer <bitu.network@gmail.com>
2026-09-20 22:27:44 +0330

    rename src/web/public/ to src/http/external/

---
+++ src/http/external/app.js
@@ -0,0 +1,43 @@
+// file: src/http/external/app.js
+// filename: /src/www/app.js
+
+const messageBox =
+    document.getElementById("message");
+
+const signButton =
+    document.getElementById("signButton");
+
+const status =
+    document.getElementById("status");
+
+
+function createLoginMessage() {
+
+    const now =
+        new Date()
+            .toISOString()
+            .replace("T", " ")
+            .replace("Z", " UTC");
+
+    return (
+        "I want to login to bitu-network.com at "
+        + now
+    );
+
+}
+
+
+const loginMessage =
+    createLoginMessage();
+
+
+messageBox.textContent =
+    loginMessage;
+
+
+signButton.onclick = function () {
+
+    status.textContent =
+        "Waiting for wallet signature...";
+
+};
